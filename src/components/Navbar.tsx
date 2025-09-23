096e39a236d23a02f6dfd704c4754161f0ec118b
aksel <aksel@metadot.com>
2025-09-23 18:45:44 -0500

    feat: enhance navbar styling and add dropdown animations

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar() {
       </Link>
 
       {/* Navigation */}
-      <NavigationMenu.List className="flex gap-6">
+      <NavigationMenu.List className="flex ">
         <NavigationMenu.Item>
           <NavigationMenu.Link asChild>
             <Link className="nav-link" href="/">
